35ef870461c666a70e2c6db50662df5f7fd5dbca
ldroberts02 <ldroberts02@gmail.com>
2024-02-06 15:09:59 -0600

    Sample Menu
    Menu opens on tilde key, 10 units from camera, no rotation yet.

---
+++ Assets/LukeFolder/MenuWork/MenuTestScript.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.UI;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class MenuTestScript : MonoBehaviour
+{
+    public GameObject canvasObject;
+    Camera gameCamera;
+    bool keyDown = false; //for the keypress toggle for the menu
+    public float menuDistance = 5.0f; //meant to push the z value of the menu away from camera
+    void Awake()
+    {
+        gameCamera = Camera.main;
+    }
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+        if (Input.GetKeyDown("`") && keyDown == false)
+        {
+                if (canvasObject.activeSelf == true && keyDown == false)
+                {
+                    canvasObject.SetActive(false);
+                    keyDown = true;
+                }
+                if (canvasObject.activeSelf == false && keyDown == false)
+                {
+                    canvasObject.SetActive(true);
+                    keyDown = true;
+                }
+                
+        }
+        if (Input.GetKeyUp("`"))
+        {
+            keyDown = false;
+        }
+
+        if(canvasObject.activeSelf)
+        {
+            this.transform.position = gameCamera.transform.position + new Vector3(0,0,menuDistance);
+        }
+
+
+
+    }
+    public void ButtonClick()
+    {
+        Debug.Log("Clicked");
+    }
+}
