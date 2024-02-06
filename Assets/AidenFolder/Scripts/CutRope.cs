f86a233b7bc4dd22c91c63354eef34594d54a249
Aiden Thacker <aidenthacker86@gmail.com>
2024-02-06 15:35:44 -0600

    Imported a realistic rope and working on cutting it in half

---
+++ Assets/AidenFolder/Scripts/CutRope.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutRope : MonoBehaviour
+{
+    public void OnTriggerEnter(Collider collider)
+    {
+        //collider.gameObject.transform.position;
+    }
+}
