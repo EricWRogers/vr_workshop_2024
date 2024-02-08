91c7ac41e9264ae661622c16035265127268d335
Aiden Thacker <aidenthacker86@gmail.com>
2024-02-07 23:34:56 -0600

    I got the Draw Bridge working, but still Working on Rope

--- Assets/AidenFolder/Scripts/CutRope.cs
+++ Assets/AidenFolder/Scripts/CutRope.cs
@@ -6,6 +6,12 @@ public class CutRope : MonoBehaviour
 {
     public void OnTriggerEnter(Collider collider)
     {
-        //collider.gameObject.transform.position;
+        if(collider.CompareTag("AidensArrow"))
+        {
+            //Debug.Log(collider.gameObject.transform.position);
+            Debug.Log("Aidens Arrow Collided");
+            Destroy(this.gameObject);
+        }
+        
     }
 }
---
+++ Assets/AidenFolder/Scripts/TestDrawBridgeFunctionality.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestDrawBridgeFunctionality : MonoBehaviour
+{
+    public GameObject rope;
+    public GameObject ropeTwo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if(rope == null)
+       {
+            Debug.Log("The space key was pressed");
+            Destroy(this.gameObject);
+       }
+    }
+}
