85563242738e7756173f2468ea590660d7e74b9f
Colby Thompson <colbykade626@gmail.com>
2024-03-09 15:15:40 -0600

    Even better quiver

--- Assets/ColbyFolder/Scripts/Player/QuiverFollow.cs
+++ Assets/ColbyFolder/Scripts/Player/QuiverFollow.cs
@@ -2,14 +2,15 @@ using UnityEngine;
 
 public class QuiverFollow : MonoBehaviour
 {
+    public Transform playerOrigin;
     public Transform playerCam;
-    //public float quiverX = 0f;
-    //public float quiverY = -0.8f;
-    //public float quiverZ = 0f;
+    public float quiverX = 0f;
+    public float quiverY = -0.8f;
+    public float quiverZ = 0f;
 
     private void Update()
     {
-        //transform.position = new Vector3(transform.position.x + playerCam.position.x + quiverX, transform.position.y + playerCam.position.y + quiverY, transform.position.z + playerCam.position.z + quiverZ);
+        transform.position = new Vector3(playerOrigin.position.x + playerCam.position.x + quiverX, playerOrigin.position.y + playerCam.position.y + quiverY, playerOrigin.position.z + playerCam.position.z + quiverZ);
         transform.rotation = Quaternion.Euler(0f, transform.rotation.y + playerCam.rotation.eulerAngles.y - 90, 0f);
     }
 }
