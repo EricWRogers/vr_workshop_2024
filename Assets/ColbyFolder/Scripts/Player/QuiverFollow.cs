f41759f28d690d922991669fd94f5ba6784442a5
Colby Thompson <colbykade626@gmail.com>
2024-04-03 22:23:31 -0500

    Fixed bow and quiver

--- Assets/ColbyFolder/Scripts/Player/QuiverFollow.cs
+++ Assets/ColbyFolder/Scripts/Player/QuiverFollow.cs
@@ -2,7 +2,6 @@ using UnityEngine;
 
 public class QuiverFollow : MonoBehaviour
 {
-    public Transform playerOrigin;
     public Transform playerCam;
     public float quiverX = 0f;
     public float quiverY = -0.8f;
@@ -10,7 +9,7 @@ public class QuiverFollow : MonoBehaviour
 
     private void Update()
     {
-        transform.position = new Vector3(playerOrigin.position.x + playerCam.position.x, playerOrigin.position.y + playerCam.position.y, playerOrigin.position.z + playerCam.position.z);
+        transform.position = new Vector3(playerCam.position.x + quiverX, playerCam.position.y + quiverY, playerCam.position.z + quiverZ);
         transform.rotation = Quaternion.Euler(0f, transform.rotation.y + playerCam.rotation.eulerAngles.y - 90, 0f);
     }
 }
