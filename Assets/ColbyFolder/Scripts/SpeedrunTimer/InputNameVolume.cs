d6159867bf03c17bf6cd792262de0283c8f32252
Colby Thompson <colbykade626@gmail.com>
2024-04-17 09:44:05 -0500

    WASD player movement freezes when inputting highscore name

--- Assets/ColbyFolder/Scripts/SpeedrunTimer/InputNameVolume.cs
+++ Assets/ColbyFolder/Scripts/SpeedrunTimer/InputNameVolume.cs
@@ -1,14 +1,41 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class InputNameVolume : MonoBehaviour
 {
+    private float playerMovespeed = 5.0f;
+    private GameObject player;
+
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //Turn off player movement
+            player = other.gameObject;
+            if (player.GetComponent<FPSController>() != null)
+            {
+                playerMovespeed = player.GetComponent<FPSController>().walkspeed;
+                player.GetComponent<FPSController>().walkspeed = 0.0f;
+            }
+            else
+            {
+                playerMovespeed = player.GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed;
+                player.GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed = 0.0f;
+            }
             EventSystem.current.SetSelectedGameObject(gameObject);
         }
+    }
+
+    public void PlayerCanMove()
+    {
+        if (player.GetComponent<FPSController>() != null)
+        {
+            player.GetComponent<FPSController>().walkspeed = playerMovespeed;
+        }
+        else
+        {
+            player.GetComponent<ActionBasedContinuousMoveProvider>().moveSpeed = playerMovespeed;
+        }
+        EventSystem.current.SetSelectedGameObject(null);
     }
 }
