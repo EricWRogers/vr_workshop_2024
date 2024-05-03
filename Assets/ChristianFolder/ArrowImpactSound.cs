0705c48a6b10b9038495c225daf3e5fa0eb346e3
Christian Wiggins <Christian.wiggins0@gmail.com>
2024-05-03 18:08:10 -0500

    Arrow splashes in water, explosive barrels make sound, fixed arrow sound

--- Assets/ChristianFolder/ArrowImpactSound.cs
+++ Assets/ChristianFolder/ArrowImpactSound.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class ArrowImpactSound : MonoBehaviour
 {
+
+    private int soundLimit = 1;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -19,13 +22,20 @@ public class ArrowImpactSound : MonoBehaviour
 
     void OnCollisionEnter(Collision other)
     {
-        if (other.gameObject.CompareTag("Target")){
-            AudioManager.instance.PlayOnObject("Target_hit", gameObject);
-        }
-
-        if (!other.gameObject.CompareTag("Bow") && !other.gameObject.CompareTag("Player") && !other.gameObject.CompareTag("Target") && !other.gameObject.CompareTag("Arrow"))
+        if (soundLimit == 1)
         {
-            AudioManager.instance.PlayOnObject("Stone_Impact", gameObject);
+            if (other.gameObject.CompareTag("Target")){
+                AudioManager.instance.PlayOnObject("Target_hit", gameObject);
+            }
+
+            if (other.gameObject.CompareTag("Water")){
+                AudioManager.instance.PlayOnObject("Water_splash", gameObject);
+            }
+
+            if (!other.gameObject.CompareTag("Bow") && !other.gameObject.CompareTag("Player") && !other.gameObject.CompareTag("Target") && !other.gameObject.CompareTag("Arrow") && !other.gameObject.CompareTag("Water")){
+                AudioManager.instance.PlayOnObject("Stone_Impact", gameObject);
+            }
+        soundLimit -= 1;
         }
     }
 
