3c1aa2bb6759c01d632bd3d8a4d81ee2caf8713d
Carson Burnham <gaster.burnham@gmail.com>
2024-05-07 23:26:54 -0500

    Completed target glow and global bloom
    both wall and floor targets have prefab variants in my folder, as well as a prefab for global bloom that needs to go on the player with post processing turned on for the cameras

--- Assets/CarsonFolder/scripts/TimerTargets/New Scripts/UpdatedTargetLogic.cs
+++ Assets/CarsonFolder/scripts/TimerTargets/New Scripts/UpdatedTargetLogic.cs
@@ -9,16 +9,22 @@ public class UpdatedTargetLogic : MonoBehaviour
     public Material hitMaterial; //To show feedback to the player they hit the target
     public Material winMaterial; //To show feedback to the player they won
     private Material startingMaterial; //To go back to once the timer runs out
-    public GameObject TargetGlow;
+    private Light TargetGlow;
     public float MaxIntensity = 4.0f;
     public float MinIntensity = 0.0f;
-
+    public float CurrentIntensity = 0.0f;
+    private float IntensityChangeRate;
+    
 
     private void Start()
     {
         meshRenderer = gameObject.GetComponent<MeshRenderer>();
         startingMaterial = meshRenderer.material;
-        
+        TargetGlow = GetComponentInChildren<Light>();
+        CurrentIntensity = MinIntensity;
+        TargetGlow.intensity = CurrentIntensity;
+        IntensityChangeRate = MaxIntensity / timerDuration;
+
     }
 
     public void StartPuzzleSolver()
@@ -26,6 +32,8 @@ public class UpdatedTargetLogic : MonoBehaviour
         timer = timerDuration;
         meshRenderer.material = hitMaterial;
         timerManager.AddTargetToList(this);
+        CurrentIntensity = MaxIntensity;
+        
         
     }
 
@@ -43,6 +51,9 @@ public class UpdatedTargetLogic : MonoBehaviour
         if (timer > 0.0f)
         {
             timer -= Time.deltaTime;
+            CurrentIntensity -= IntensityChangeRate * Time.deltaTime;
+            CurrentIntensity = Mathf.Clamp(CurrentIntensity, MinIntensity, MaxIntensity);
+            TargetGlow.intensity = CurrentIntensity;
 
             if (timer <= 0.0f)
             {
@@ -57,7 +68,7 @@ public class UpdatedTargetLogic : MonoBehaviour
         {
             timerManager.RemoveTargetFromList(this);
             meshRenderer.material = startingMaterial;
-            
+            TargetGlow.intensity = 0.0f;
         }
     }
 }
