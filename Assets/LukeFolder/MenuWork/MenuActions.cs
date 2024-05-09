3182a8d2c67db822cef988a3d80433d8ef866196
ldroberts02 <ldroberts02@gmail.com>
2024-05-09 11:39:26 -0500

    Menu Work not done, pushing for cloud access

---
+++ Assets/LukeFolder/MenuWork/MenuActions.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuActions : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void OnPress()
+    {
+        Debug.Log("Hey");
+        SceneManager.LoadScene(1);
+    }
+    public void CloseMenu()
+    {
+        Debug.Log("PRESSED CLOSE");
+    }
+    public void OptionsButton()
+    {
+        Debug.Log("PRESSED OPTIONS");
+    }
+    public void ExitButton()
+    {
+        Debug.Log("PRESSED RETURN");
+    }
+}
