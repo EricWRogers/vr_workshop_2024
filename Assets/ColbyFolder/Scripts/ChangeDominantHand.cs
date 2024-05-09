167904d0f14f0b0bf65520e4ed5469b4f5fa3976
Colby Thompson <colbykade626@gmail.com>
2024-05-08 20:36:13 -0500

    Remembers which hand it is supposed to do but only partially works in new scenes

--- Assets/ColbyFolder/Scripts/ChangeDominantHand.cs
+++ Assets/ColbyFolder/Scripts/ChangeDominantHand.cs
@@ -2,54 +2,17 @@ using UnityEngine;
 
 public class ChangeDominantHand : MonoBehaviour
 {
-    private VRInputs[] inputs;
-    private Quiver quiver;
-    private Vector3 rightQuiverRotation = new Vector3(-60f, 90f, -90f);
-    private Vector3 leftQuiverRotation = new Vector3(-120f, 90f, -90f);
-
     public void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
             if (GameManager.Instance.rightHandMode)
             {
-                inputs = other.GetComponents<VRInputs>();
-                //Change lefthand bow spawn to right hand
-                inputs[0].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Right;
-                //Swap what hand the bow spawns at
-                (other.GetComponent<SpawnBow>().rightController, other.GetComponent<SpawnBow>().leftController) = (other.GetComponent<SpawnBow>().leftController, other.GetComponent<SpawnBow>().rightController);
-                //Change arrow spawning from right hand to left hand
-                inputs[1].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Left;
-                //Swap hands in SpawnArrowVR
-                (other.GetComponent<SpawnArrowVR>().leftController, other.GetComponent<SpawnArrowVR>().rightController) = (other.GetComponent<SpawnArrowVR>().rightController, other.GetComponent<SpawnArrowVR>().leftController);
-                //Access the quiver
-                quiver = other.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<Quiver>();
-                //Swap hands for the quiver
-                (quiver.leftController, quiver.rightController) = (quiver.rightController, quiver.leftController);
-                //Flip the quiver model
-                Transform quiverPouch = other.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).transform;
-                quiverPouch.localEulerAngles = leftQuiverRotation;
-                GameManager.Instance.rightHandMode = false;
+                GameManager.Instance.ChangeToLeftHandMode();
             }
             else
             {
-                inputs = other.GetComponents<VRInputs>();
-                //Change righthand bow spawn to left hand
-                inputs[0].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Left;
-                //Swap what hand the bow spawns at
-                (other.GetComponent<SpawnBow>().rightController, other.GetComponent<SpawnBow>().leftController) = (other.GetComponent<SpawnBow>().leftController, other.GetComponent<SpawnBow>().rightController);
-                //Change arrow spawning from left hand to right hand
-                inputs[1].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Right;
-                //Swap hands in SpawnArrowVR
-                (other.GetComponent<SpawnArrowVR>().leftController, other.GetComponent<SpawnArrowVR>().rightController) = (other.GetComponent<SpawnArrowVR>().rightController, other.GetComponent<SpawnArrowVR>().leftController);
-                //Access the quiver
-                quiver = other.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<Quiver>();
-                //Swap hands for the quiver
-                (quiver.leftController, quiver.rightController) = (quiver.rightController, quiver.leftController);
-                //Flip the quiver model
-                Transform quiverPouch = other.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).transform;
-                quiverPouch.localEulerAngles = rightQuiverRotation;
-                GameManager.Instance.rightHandMode = true;
+                GameManager.Instance.ChangeToRightHandMode();
             }
         }
     }
--- Assets/ColbyFolder/Scripts/Utility/GameManager.cs
+++ Assets/ColbyFolder/Scripts/Utility/GameManager.cs
@@ -1,9 +1,15 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public static GameManager Instance;
+    private GameObject player;
+    private Quiver quiver;
+    private Vector3 rightQuiverRotation = new Vector3(-60f, 90f, -90f);
+    private Vector3 leftQuiverRotation = new Vector3(-120f, 90f, -90f);
     public bool rightHandMode = true;
+
     private void Awake()
     {
         if (Instance == null)
@@ -15,5 +21,58 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
         DontDestroyOnLoad(gameObject);
+
+        SceneManager.sceneLoaded += OnSceneLoaded;
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (!rightHandMode)
+        {
+            ChangeToLeftHandMode();
+        }
+    }
+
+    public void ChangeToRightHandMode()
+    {
+        VRInputs[] inputs = player.GetComponents<VRInputs>();
+        //Change righthand bow spawn to left hand
+        inputs[0].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Left;
+        //Swap what hand the bow spawns at
+        (player.GetComponent<SpawnBow>().rightController, player.GetComponent<SpawnBow>().leftController) = (player.GetComponent<SpawnBow>().leftController, player.GetComponent<SpawnBow>().rightController);
+        //Change arrow spawning from left hand to right hand
+        inputs[1].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Right;
+        //Swap hands in SpawnArrowVR
+        (player.GetComponent<SpawnArrowVR>().leftController, player.GetComponent<SpawnArrowVR>().rightController) = (player.GetComponent<SpawnArrowVR>().rightController, player.GetComponent<SpawnArrowVR>().leftController);
+        //Access the quiver
+        quiver = player.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<Quiver>();
+        //Swap hands for the quiver
+        (quiver.leftController, quiver.rightController) = (quiver.rightController, quiver.leftController);
+        //Flip the quiver model
+        Transform quiverPouch = player.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).transform;
+        quiverPouch.localEulerAngles = rightQuiverRotation;
+        rightHandMode = true;
+    }
+
+    public void ChangeToLeftHandMode() 
+    {
+        VRInputs[] inputs = FindObjectOfType<SpawnArrowVR>().GetComponents<VRInputs>();
+        //Change lefthand bow spawn to right hand
+        inputs[0].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Right;
+        //Swap what hand the bow spawns at
+        (player.GetComponent<SpawnBow>().rightController, player.GetComponent<SpawnBow>().leftController) = (player.GetComponent<SpawnBow>().leftController, player.GetComponent<SpawnBow>().rightController);
+        //Change arrow spawning from right hand to left hand
+        inputs[1].deviceCharacteristics = UnityEngine.XR.InputDeviceCharacteristics.Left;
+        //Swap hands in SpawnArrowVR
+        (player.GetComponent<SpawnArrowVR>().leftController, player.GetComponent<SpawnArrowVR>().rightController) = (player.GetComponent<SpawnArrowVR>().rightController, player.GetComponent<SpawnArrowVR>().leftController);
+        //Access the quiver
+        quiver = player.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).GetComponent<Quiver>();
+        //Swap hands for the quiver
+        (quiver.leftController, quiver.rightController) = (quiver.rightController, quiver.leftController);
+        //Flip the quiver model
+        Transform quiverPouch = player.transform.GetChild(0).transform.GetChild(0).transform.GetChild(0).transform.GetChild(2).transform;
+        quiverPouch.localEulerAngles = leftQuiverRotation;
+        rightHandMode = false;
     }
 }
