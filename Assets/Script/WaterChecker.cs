56a692b2838f89b8b305851e64a507b0ffda544b
dylanHilliard <ghilliard69420@gmail.com>
2025-04-17 12:49:07 -0500

    Water Checker Script

---
+++ Assets/Script/WaterChecker.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaterChecker : MonoBehaviour
+{
+
+    private GameObject player;
+    public GameObject water;
+    public float height = 17.0f;
+    private bool aboveHeight = false;
+    void Start()
+    {
+     player = GameObject.FindWithTag("Player");   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        aboveHeight = (player.transform.position.y >height) ? true : false;
+        gameObject.SetActive(aboveHeight);
+    }
+}
