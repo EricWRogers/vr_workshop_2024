72f25e991885eb9e9c3033aa9f5a507562af2936
Garrett <garwad9@gmail.com>
2025-05-02 16:02:46 -0500

    the knife be throwing

--- Assets/Script/Player/SpawnKnife.cs
+++ Assets/Script/Player/SpawnKnife.cs
@@ -7,7 +7,7 @@ public class SpawnKnife : MonoBehaviour
     public GameObject knifePrefab;
     [HideInInspector]
     public GameObject knife;
-    private Bullet bulletScript;
+    //private Bullet bulletScript;
 
     [HideInInspector]
     public GameObject rightController;
@@ -15,38 +15,79 @@ public class SpawnKnife : MonoBehaviour
     public GameObject leftController;
 
     public bool knifeSpawned = false;
-    private ThrowingKnife TKScript;
 
     public bool canTeleport = true;
+    public float throwForceMultiplier = 1.5f;
+
+    private XRBaseInteractor interactor;
+    private Vector3 lastPosition;
+    private Vector3 velocity;
     // Start is called before the first frame update
+
+    void Start()
+    {
+        interactor = rightController.GetComponent<XRBaseInteractor>();
+    }
+
+    void Update()
+    {
+        // Track velocity manually
+        if (knifeSpawned && knife != null)
+        {
+            velocity = (rightController.transform.position - lastPosition) / Time.deltaTime;
+            lastPosition = rightController.transform.position;
+        }
+    }
     public void SpawnAKnife()
     {
-        if (canTeleport)
+        if (canTeleport && !knifeSpawned)
+        {
+            knife = Instantiate(knifePrefab, rightController.transform.position, transform.rotation);
+
+#pragma warning disable CS0618
+            interactor.StartManualInteraction(knife.GetComponent<XRGrabInteractable>());
+#pragma warning restore CS0618
+
+            knifeSpawned = true;
+        }
+    }
+    /*public void SpawnAKnife()
+    {
+        if (canTeleport && !knifeSpawned)
         {
             if (!knifeSpawned)
             {
                 knife = Instantiate(knifePrefab, rightController.transform.position, transform.rotation);
                 //bulletScript = knife.GetComponent<Bullet>();
-#pragma warning disable CS0618 // Type or member is obsolete, this line removes the error message
+    #pragma    warning disable CS0618 // Type or member is obsolete, this line removes the error message
                 rightController.GetComponent<XRBaseInteractor>().StartManualInteraction(knife.GetComponent<XRGrabInteractable>());
-#pragma warning restore CS0618 // Type or member is obsolete, this line resumes error messages
+    #pragma    warning restore CS0618 // Type or member is obsolete, this line resumes error messages
                 knifeSpawned = true;
-                TKScript = knife.GetComponent<ThrowingKnife>();
             }
         }
-    }
+    }*/
     public void ThrowKnife()
     {
-        rightController.GetComponent<XRBaseInteractor>().EndManualInteraction();
+        if (knife == null) return;
 
-        float mag = Vector3.Magnitude(TKScript.curPos - TKScript.lastPos);
-        Vector3 direction = (TKScript.curPos - TKScript.lastPos).normalized;
+        interactor.EndManualInteraction();
 
-        knife.transform.rotation = Quaternion.LookRotation(direction);
-        knife.GetComponent<Bullet>().speed = mag * 100;
-        knife.GetComponent<Bullet>().enabled = true;
+        ThrowingKnife throwingKnife = knife.GetComponent<ThrowingKnife>();
+        if (throwingKnife != null)
+        {
+            Vector3 throwDir = velocity.normalized;
+            float throwStrength = velocity.magnitude * throwForceMultiplier;
+            throwingKnife.Throw(throwDir, throwStrength);
+        }
+
         knifeSpawned = false;
     }
+    /*public void ThrowKnife()
+    {
+        rightController.GetComponent<XRBaseInteractor>().EndManualInteraction();
+        knife.GetComponent<Bullet>().enabled = true;
+        knifeSpawned = false;
+    }*/
 
     public void TeleportTrue()
     {
